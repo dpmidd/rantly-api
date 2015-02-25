bd25f0b90fa814d797595f5d2526578893150648
Dmidd87 <dmidd87@gmail.com>
2015-02-25 13:57:19 -0700

    Added users show page controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'pp'
 
 RSpec.describe UsersController, type: :controller do
   describe "#index" do
@@ -11,11 +12,30 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
+  describe "#show" do
+    it "returns the json of the selected user" do
+      user1 = create_user
+      user2 = create_user
+
+      get :show, id: user1.id
+
+      expect(response.status).to be(200)
+      response_array = JSON.parse(response.body)
+      expect(response_array["first_name"]).to eq(user1.first_name)
+
+      get :show, id: user2.id
+
+      expect(response.status).to be(200)
+      response_array = JSON.parse(response.body)
+      expect(response_array["first_name"]).to eq(user2.first_name)
+    end
+  end
+
   describe "#update" do
-    pending
+
   end
 
   describe "#destroy" do
-    pending
+
   end
 end
