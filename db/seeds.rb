b1dcae19ef5ca8fc138949ae6a81f263f139fdbe
Dmidd87 <dmidd87@gmail.com>
2015-02-26 10:15:40 -0700

    Edited the seed file so that each user was getting a rant instead of one user getting all the rants

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,19 +3,15 @@ Rant.delete_all
 
 10.times do
 
-@user = User.create!({
+user = User.create!({
   email: Faker::Internet.email,
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
   password_digest: 'password',
 })
 
-end
-
-10.times do
-
 Rant.create!({
-  user_id: @user.id,
+  user_id: user.id,
   title: Faker::Name.title,
   body: Faker::Lorem.sentence
 })
