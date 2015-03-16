a8e0b1f8ed4946096650aeb2a41cf5cac73af745
Dmidd87 <dmidd87@gmail.com>
2015-03-16 09:57:40 -0600

    fixed seed file according to devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ user = User.create!({
   email: Faker::Internet.email,
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
-  password_digest: 'password',
+  password: 'password',
 })
 
 Rant.create!({
