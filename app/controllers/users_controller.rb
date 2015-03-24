7ebdc743a3a37c678b6457a5037cf47d4d631718
Dmidd87 <dmidd87@gmail.com>
2015-03-24 09:23:24 -0600

    Added create to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,14 @@ class UsersController < ApplicationController
     render json: User.find(params[:id])
   end
 
+  def create
+    @user = User.new(user_params)
+    if  @user.save
+      sign_in @user
+      render json: @user
+    end
+  end
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
