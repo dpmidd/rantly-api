984ebb047236741a51b1c231ae17e314d6f09504
Dmidd87 <dmidd87@gmail.com>
2015-03-24 10:06:32 -0600

    refactored application controller and added if clause to devise controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,25 @@
 class ApplicationController < ActionController::Base
   before_filter :authenticate_user_from_token!
-  before_filter :authenticate_user!
+  before_filter :configure_permitted_parameters!, if: :devise_controller?
+
+  protect_from_forgery with: :null_session
 
   private
 
   def authenticate_user_from_token!
     authenticate_with_http_token do |token, options|
-    uemail = options[:email].presence
-    user       = user_email && User.find_by_email(user_email)
+      user_email = options[:email].presence
+      user = user_email && User.find_by_email(user_email)
 
       if user && Devise.secure_compare(user.authentication_token, token)
         sign_in user, store: false
       end
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name]
   end
 end
