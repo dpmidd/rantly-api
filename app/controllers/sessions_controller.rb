38c0d42d5c717265d5525427886a691213de1c84
Dmidd87 <dmidd87@gmail.com>
2015-03-24 09:33:57 -0600

    Added the right authorization to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,17 @@
 class SessionsController < Devise::SessionsController
+  skip_before_filter :configure_permitted_parameters!
+
   respond_to :html, :json
 
   def create
     super do |user|
       if request.format.json?
         data = {
-          token:      user.authentication_token,
+          token: user.authentication_token,
           email: user.email,
+          user_id: user.id,
+          user_first_name: user.first_name,
+          user_last_name: user.last_name
         }
         render json: data, status: 201 and return
       end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+  before_filter :authenticate_user!, only: [:delete]
+  skip_before_filter :verify_authenticity_token
+
   def index
     render json: User.all
   end
