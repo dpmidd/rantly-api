5eec2afb94cebce283825afa3a245a0381330417
Dmidd87 <dmidd87@gmail.com>
2015-03-24 15:36:24 -0600

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,38 @@ user = User.create!({
   password: 'password',
 })
 
+user2 = User.create!({
+  email: 'user2@example.com',
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  password: 'password',
+})
+
+user3 = User.create!({
+  email: 'user3@example.com',
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  password: 'password',
+})
+
+Rant.create!({
+  user_id: user.id,
+  title: Faker::Name.title,
+  body: Faker::Lorem.sentence
+})
+
+Rant.create!({
+  user_id: user.id,
+  title: Faker::Name.title,
+  body: Faker::Lorem.sentence
+})
+
+Rant.create!({
+  user_id: user.id,
+  title: Faker::Name.title,
+  body: Faker::Lorem.sentence
+})
+
 Rant.create!({
   user_id: user.id,
   title: Faker::Name.title,
