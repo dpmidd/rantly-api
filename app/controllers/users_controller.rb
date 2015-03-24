9e7fb230008b2e790653829a3f140536ba94a3ae
Dmidd87 <dmidd87@gmail.com>
2015-03-24 13:18:39 -0600

    Added errors and figure out sign in is working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,10 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      sign_in @user
+      if @user.save
       render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
 
