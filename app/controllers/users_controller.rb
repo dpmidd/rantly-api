9bff16bf3a1f57bd2486cfb09b08d5385ec2a6c4
Dmidd87 <dmidd87@gmail.com>
2015-03-24 11:52:01 -0600

    Deleted old serializer folder with type and generated new one

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if  @user.save
+    if @user.save
       sign_in @user
       render json: @user
     end
